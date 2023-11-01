dfbba529633f2ce1b18811e2a91f5611170baf7c
root <root@LAPTOP-8BCHHM7B.localdomain>
2023-11-01 14:05:58 -0600

    tercer parrafo

--- global_configuration.php
+++ global_configuration.php
@@ -6,6 +6,7 @@ $email = 'user@example.com';
 $password = '';
 $parrafo_1 = 'Este es el ejemplo de un párrafo obtenido desde archivo de configuración';
 $parrafo_2 = 'Segundo párrafo en Ubuntu';
+$parrafo_3 = 'Un párrafo más';
 
 if (file_exists('configuration.php')) {
 	include_once('configuration.php');
