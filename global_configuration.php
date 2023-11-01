af6b5d3f707acd91a81609a8b2df005bab93f5c7
Robinson Garcia <edifactmxrobinsongarcia@gmail.com>
2023-11-01 13:14:23 -0600

    inicializando variables de configuracion

--- global_configuration.php
+++ global_configuration.php
@@ -1,10 +1,10 @@
 <?php
 
+$name = 'App GitHub';
+$user_name = 'root';
+$email = 'user@example.com';
+$password = '';
+	
 if (file_exists('configuration.php')) {
 	include_once('configuration.php');
-} else {
-	$name = 'App GitHub';
-	$user_name = 'root';
-	$email = 'user@example.com';
-	$password = '';
-}
+} 
