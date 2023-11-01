0575a6135c6ca5538db527e65cd79368707ea058
root <root@LAPTOP-8BCHHM7B.localdomain>
2023-11-01 13:52:58 -0600

    segundo parrafo en ubuntu

--- global_configuration.php
+++ global_configuration.php
@@ -5,6 +5,7 @@ $user_name = 'root';
 $email = 'user@example.com';
 $password = '';
 $parrafo_1 = 'Este es el ejemplo de un párrafo obtenido desde archivo de configuración';
+$parrafo_2 = 'Segundo párrafo en Ubuntu';
 
 if (file_exists('configuration.php')) {
 	include_once('configuration.php');
